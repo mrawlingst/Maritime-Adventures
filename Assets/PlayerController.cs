b776ebd7924425c9226ffbddcdfae6d9fe9acf89
Michael Rawlings <mrawlingst@gmail.com>
2017-01-21 09:54:12 -0500

    start of the adventure

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed = 20;
+    public float RotateSpeed = 50;
+    public bool Movable = true;
+
+	void Update()
+    {
+        if (!Movable)
+            return;
+        // Movement with keyboard
+        float rotate = Input.GetAxis("Horizontal") * Time.deltaTime * RotateSpeed;
+        float move = Input.GetAxis("Vertical") * Time.deltaTime * MoveSpeed;
+
+        transform.Rotate(0, rotate, 0);
+        transform.Translate(0, 0, move);
+	}
+}
---
+++ Assets/TriggerCutscene.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TriggerCutscene : MonoBehaviour {
+
+    public Text text;
+    public GameObject image;
+    public GameObject background;
+    public GameObject toptext;
+    public PlayerController playercontroller;
+    public string message;
+
+    void OnTriggerEnter(Collider other)
+    {
+        text.text = message;
+        playercontroller.Movable = false;
+        image.SetActive(true);
+        toptext.SetActive(true);
+        background.SetActive(true);
+        StartCoroutine(BlockInput());
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+    }
+
+    IEnumerator BlockInput()
+    {
+        yield return new WaitForSeconds(5.0f);
+        playercontroller.Movable = true;
+        image.SetActive(false);
+        toptext.SetActive(false);
+        background.SetActive(false);
+    }
+}
